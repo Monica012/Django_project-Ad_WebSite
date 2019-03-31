8693494c1b23f37eb76a36c29a6210a47439702e
Nai <nai@0587357158.wireless.umich.net>
2019-03-31 18:50:32 -0400

    first commit

---
+++ ads/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path('', views.AdListView.as_view()),
+    path('ads', views.AdListView.as_view(), name='ads'),
+    path('ad/<int:pk>', views.AdDetailView.as_view(), name='ad_detail'),
+    path('ad/create',
+        views.AdCreateView.as_view(success_url=reverse_lazy('ads')), name='ad_create'),
+    path('ad/<int:pk>/update',
+        views.AdUpdateView.as_view(success_url=reverse_lazy('ads')), name='ad_update'),
+    path('ad/<int:pk>/delete',
+        views.AdDeleteView.as_view(success_url=reverse_lazy('ads')), name='ad_delete'),
+]
